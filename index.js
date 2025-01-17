3249322995d702446e17aff5d1e67d0e2c86f860
luiscr918 <lc9107040@gmail.com>
2025-01-16 21:50:16 -0500

    conectado correctamente a base de datos

--- index.js
+++ index.js
@@ -1,9 +1,58 @@
 const express = require("express");
+
+const mysql = require("mysql2");
+
 const app = express();
+
+let conexion = mysql.createConnection({
+  host: "localhost",
+  database: "eilish_vibes",
+  user: "root",
+  password: "Argentina_123",
+});
+
 app.set("view engine", "ejs");
+//datos que vienen de otra pagina u otra ubicacion antes de las rutas
+app.use(express.static("src"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", function (req, res) {
   res.render("singUp");
+});
+
+app.post("/validar", function (req, res) {
+  const datos = req.body;
+  console.log(datos);
+
+  let correo = datos.email;
+  let password = datos.contrasenia;
+
+  let buscar = "SELECT *FROM usuarios WHERE email='" + correo + "'";
+
+  conexion.query(buscar, function (error, row) {
+    if (error) {
+      throw error;
+    } else {
+      if (row.length > 0) {
+        console.log("No se puede registrar usuario ya existe");
+      } else {
+        let registrar =
+          "INSERT INTO usuarios (email,contrasenia) VALUES ('" +
+          correo +
+          "','" +
+          password +
+          "')";
+        conexion.query(registrar, function (error) {
+          if (error) {
+            throw error;
+          } else {
+            console.log("DATOS ALMACENADOS CORRECTAMENTE");
+          }
+        });
+      }
+    }
+  });
 });
 
 app.listen(3000, function () {
--- src/js/navBar.js
+++ src/js/navBar.js
@@ -171,7 +171,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
                               class="w-full text-white bg-primary-600 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign
                               in</button>
                           <p class="text-sm font-light text-white dark:text-white ">
-                              Don’t have an account yet? <a href="src/pages/signUp.ejs"
+                              Don’t have an account yet? <a href="http://localhost:3000"
                                   class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
                           </p>
                       </form>
